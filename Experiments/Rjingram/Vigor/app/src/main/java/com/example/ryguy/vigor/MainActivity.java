925fe3d88cfe15e7a7197e88fcb84cac79c660d3
Ryan <rjingram@iastate.edu>
2018-09-20 14:44:11 -0500

    Start User Profile

---
+++ Experiments/Rjingram/Vigor/app/src/main/java/com/example/ryguy/vigor/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ryguy.vigor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.buttonMessage);
+        b2 = (Button) findViewById(R.id.buttonNext);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello Android!", Toast.LENGTH_LONG).show();
+            }
+        } );
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
