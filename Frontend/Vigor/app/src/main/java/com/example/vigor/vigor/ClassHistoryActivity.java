7bb92d626235b0244830a3e3ba68a5f7e6f2bfa8
Kirkland Keith <kjkeith@iastate.edu>
2018-11-27 14:33:02 -0600

    Worked on class history class.

--- Frontend/Vigor/app/src/main/java/com/example/vigor/vigor/ClassHistoryActivity.java
+++ Frontend/Vigor/app/src/main/java/com/example/vigor/vigor/ClassHistoryActivity.java
@@ -4,25 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ClassHistoryActivity extends Activity {
     private String TAG = ClassViewActivity.class.getSimpleName();
-
-    private Button classHistory;
     private ListView classHistoryList;
+    private ClassWorkoutAdapter adapter;
+    private ArrayList<ClassWorkoutDataModel> workoutDataModels;
 
     private SessionController session;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_classhistory);
-//
-//        classHistory = findViewById(R.id.histButt);
-//        classWorkoutList = findViewById(R.id.listWorkouts);
+        setContentView(R.layout.activity_classhistory);
+
+        classHistoryList = findViewById(R.id.listWorkouts);
 
         session = new SessionController(getApplicationContext());
+        workoutDataModels = new ArrayList<>();
+
+        //TODO populate workoutDataModels
+
+        adapter = new ClassWorkoutAdapter(workoutDataModels, getApplicationContext());
+        classHistoryList.setAdapter(adapter);
+
+        classHistoryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ClassWorkoutDataModel activityValue = (ClassWorkoutDataModel) classHistoryList.getItemAtPosition(position);
+                //TODO Open small screen with full info of item.
+            }
+        });
     }
 }
