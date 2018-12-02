3825a7597f7fd5cfe7794a410bffe3b03dc52cd4
Benjamin Zaley <bzaley@iastate.edu>
2018-12-01 19:38:06 -0600

    Made change to instructor class handling

---
+++ Backend/Server_Control/src/main/java/com/project/classes/sendClassId.java
@@ -0,0 +1,23 @@
+package com.project.classes;
+
+public class sendClassId {
+	private int classId;
+	
+	public sendClassId() {
+		
+	}
+	
+	public sendClassId(int classId) {
+		this.classId = classId;
+	}
+
+	public int getClassId() {
+		return classId;
+	}
+
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+	
+	
+}
